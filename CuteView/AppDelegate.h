c544a3c5ac4f344b747f4419ace40a1803351cd0
leven <wen@borderxlab.com>
2018-02-06 16:13:45 +0800

    Initial Commit

---
+++ CuteView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CuteView
+//
+//  Created by leven on 2018/2/6.
+//  Copyright © 2018年 leven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CuteView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CuteView
+//
+//  Created by leven on 2018/2/6.
+//  Copyright © 2018年 leven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
